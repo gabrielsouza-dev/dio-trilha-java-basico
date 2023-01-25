7c3f4ad49073585ec3b04f71f90aa2c0bdc12e7b
gabrielsouza-dev <gabrieldasilvaesouza@gmail.com>
2023-01-25 18:38:32 -0300

    aula sobre tipos e variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //classe String que representa textos na aplicação
+
+        String meuNome = "Gabriel Souza";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
